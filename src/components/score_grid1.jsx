13ecdb994b8b21d977ef0711378f412d18c15dbe
Nat Elliott-Green <natelliottgreen34@gmail.com>
2020-01-07 17:38:50 +0000

    Add onclick functions for score grid 1 - holding text currently

--- src/components/score_grid1.jsx
+++ src/components/score_grid1.jsx
@@ -6,10 +6,42 @@ import './score_grid1.css';
 class ScoreGrid1 extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      sg1State: ''
+    }
+    this.twoMAClick = this.twoMAClick.bind(this)
+    this.threeMAClick = this.threeMAClick.bind(this)
+    this.fourMAClick = this.fourMAClick.bind(this)
+  }
 
+  twoMAClick = () => {
+    this.setState({
+      sg1State: "2MA"
+    })
+  }
+
+  threeMAClick = () => {
+    this.setState({
+      sg1State: "3MA"
+    })
+  }
+
+  fourMAClick = () => {
+    this.setState({
+      sg1State: "4MA"
+    })
   }
 
   render() {
+    let graph_choice
+
+    if (this.state.sg1State === "2MA") {
+      graph_choice = "2MA AGES GRAPHS"
+    } else if (this.state.sg1State === "3MA") {
+      graph_choice = "3MA AGES GRAPHS"
+    } else if (this.state.sg1State === "4MA") {
+      graph_choice = "4MA AGES GRAPHS"
+    }
     return (
       <div>
         <div>
@@ -17,20 +49,27 @@ class ScoreGrid1 extends React.Component {
         </div>
         <ButtonGroup vertical>
           <Button
-          id="sg1_2ma">
+          id="sg1_2ma"
+          onClick={this.twoMAClick}>
           2 Ma
           </Button>
           <Button
           id="sg1_3ma"
+          onClick={this.threeMAClick}
           >
           3 Ma
           </Button>
           <Button
           id="sg1_4ma"
+          onClick={this.fourMAClick}
           >
           4 Ma
           </Button>
         </ButtonGroup>
+        <div
+        className="graphs">
+        {graph_choice}
+        </div>
       </div>
     )
   }
