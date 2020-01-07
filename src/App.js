72318cf65463dfcb5e788ccacab6caab30cd6639
Nat Elliott-Green <natelliottgreen34@gmail.com>
2020-01-07 12:31:49 +0000

    Add onclick handler to set state based on binning method selected.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,29 @@ import './App.css';
 class App extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      binningMethod: false,
+    }
   }
+
+  stageBinsClick = () => {
+    this.setState({
+      binningMethod: "StageBins"
+    })
+  }
+
+  scoreGrid1Click = () => {
+    this.setState({
+      binningMethod: "ScoreGrid1"
+    })
+  }
+
+  scoreGrid2Click = () => {
+    this.setState({
+      binningMethod: "ScoreGrid2"
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -17,9 +39,21 @@ class App extends React.Component {
           </div>
           <ButtonToolbar aria-label="Toolbar with button groups">
             <ButtonGroup className="mr-2" aria-label="First group">
-              <Button>Stage Bins</Button>
-              <Button>Score Grid 1</Button>
-              <Button>Score Grid 2</Button>
+              <Button
+              id="stagebins"
+              onClick={this.stageBinsClick}>
+              Stage Bins
+              </Button>
+              <Button
+              id="scoregrid1"
+              onClick={this.scoreGrid1Click}>
+              Score Grid 1
+              </Button>
+              <Button
+              id="scoregrid2"
+              onClick={this.scoreGrid2Click}>
+              Score Grid 2
+              </Button>
             </ButtonGroup>
           </ButtonToolbar>
         </div>
