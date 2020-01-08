222af90bd9d17cff228f9798af554c06fc963361
Nat Elliott-Green <natelliottgreen34@gmail.com>
2020-01-08 11:43:00 +0000

    Styled the graph component intially - requires review from product owner

--- src/components/score_grid1.jsx
+++ src/components/score_grid1.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import GraphSg12Ma from './graphs/sg1_2ma.jsx';
 import './score_grid1.css';
 
@@ -45,32 +47,40 @@ class ScoreGrid1 extends React.Component {
     }
     return (
       <div>
-        <div>
-        Resolution
-        </div>
-        <ButtonGroup vertical>
-          <Button
-          id="sg1_2ma"
-          onClick={this.twoMAClick}>
-          2 Ma
-          </Button>
-          <Button
-          id="sg1_3ma"
-          onClick={this.threeMAClick}
-          >
-          3 Ma
-          </Button>
-          <Button
-          id="sg1_4ma"
-          onClick={this.fourMAClick}
-          >
-          4 Ma
-          </Button>
-        </ButtonGroup>
-        <div
-        className="graphs">
-        {graph_choice}
-        </div>
+        <Row>
+          <Col
+          sm={3}
+          className="options-holder">
+            <div>
+            Resolution
+            </div>
+            <ButtonGroup vertical>
+              <Button
+              id="sg1_2ma"
+              onClick={this.twoMAClick}>
+              2 Ma
+              </Button>
+              <Button
+              id="sg1_3ma"
+              onClick={this.threeMAClick}
+              >
+              3 Ma
+              </Button>
+              <Button
+              id="sg1_4ma"
+              onClick={this.fourMAClick}
+              >
+              4 Ma
+              </Button>
+            </ButtonGroup>
+          </Col>
+          <Col className="graph-holder">
+            <div
+            className="graphs">
+            {graph_choice}
+            </div>
+          </Col>
+        </Row>
       </div>
     )
   }
