cc15677b5dbda73e1271522b2d2a9e29b4bd8cee
Nat Elliott-Green <natelliottgreen34@gmail.com>
2020-01-10 12:19:52 +0000

    Refactored graph headers into the conditional rendering if statement.

--- src/components/graphs/sg1_graphs.jsx
+++ src/components/graphs/sg1_graphs.jsx
@@ -16,30 +16,24 @@ class Sg1Graphs extends React.Component {
     let secondary_graph
 
     if (this.props.sG1State === "2MA") {
-      main_graph = <img alt="graph-one" className="graph-one" src="M2_SQS_SG1.jpeg"></img>
-      secondary_graph = <img alt="graph-two" className="graph-two" src="M2_div_colls_SG1.jpeg"></img>
+      main_graph = <div><h2 className="main-graph-header">Primary Graph Name</h2><img alt="graph-one" className="graph-one" src="M2_SQS_SG1.jpeg"></img></div>
+      secondary_graph = <div><h2 className="second-graph-header">Secondary Graph Name</h2><img alt="graph-two" className="graph-two" src="M2_div_colls_SG1.jpeg"></img></div>
     } else if (this.props.sG1State === "3MA") {
-      main_graph = <img alt="graph-one" className="graph-one" src="M2_SQS_SG1.jpeg"></img>
-      secondary_graph = <img alt="graph-two" className="graph-two" src="M2_div_colls_SG1.jpeg"></img>
+      main_graph = <div><h2 className="main-graph-header">Primary Graph Name</h2><img alt="graph-one" className="graph-one" src="M2_SQS_SG1.jpeg"></img></div>
+      secondary_graph = <div><h2 className="second-graph-header">Secondary Graph Name</h2><img alt="graph-two" className="graph-two" src="M2_div_colls_SG1.jpeg"></img></div>
     } else if (this.props.sG1State === "4MA") {
-      main_graph = <img alt="graph-one" className="graph-one" src="M2_SQS_SG1.jpeg"></img>
-      secondary_graph = <img alt="graph-two" className="graph-two" src="M2_div_colls_SG1.jpeg"></img>
+      main_graph = <div><h2 className="main-graph-header">Primary Graph Name</h2><img alt="graph-one" className="graph-one" src="M2_SQS_SG1.jpeg"></img></div>
+      secondary_graph = <div><h2 className="second-graph-header">Secondary Graph Name</h2><img alt="graph-two" className="graph-two" src="M2_div_colls_SG1.jpeg"></img></div>
     }
     return (
       <div>
         <Container
         className="graphs-container">
           <Col className="main-graph">
-          <h2 className="main-graph-header">
-          Primary Graph Name
-          </h2>
-          {main_graph}
+            {main_graph}
           </Col>
           <Col className="secondary-graph">
-          <h2 className="second-graph-header">
-          Secondary Graph Name
-          </h2>
-          {secondary_graph}
+            {secondary_graph}
           </Col>
         </Container>
       </div>
